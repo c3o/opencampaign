f7fa2b549c5ed5c21fa1723729b4b82c5edcfa92
Christopher Clay <hello@c3o.org>
2010-04-05 08:41:41 +0200

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,35 @@
-set :application, "lif"
-set :repository,  "git+ssh://lifdev@dev.ext.soup.io/home/lifdev/git/lif"
+set :application, "opencampaign"
+set :repository,  "git@github.com:c3o/opencampaign.git"
 
 set :user, "app"
 
 set :scm, :git
-set :scm_username, "lifdev"
+set :scm_username, "c3o"
 set :git_enable_submodules, 1
-set :deploy_via, :remote_cache
-ssh_options[:forward_agent] = true
+set :user, 'arminsoyka'
+set :deploy_to, "/home/arminsoyka/test.arminsoyka.at/#{application}"
+set :use_sudo, false
+set :deploy_via, :copy
 
-role :app, "diesmal-lif.at"
-role :web, "diesmal-lif.at"
-role :db,  "diesmal-lif.at", :primary => true
+role :app, "test.arminsoyka.at"
+role :web, "test.arminsoyka.at"
 
 after  "deploy:finalize_update" do
-  run "ln -nfs #{shared_path}/uploads #{current_path}/public/images/uploads"
-  run "ln -nfs #{shared_path}/portraits #{current_path}/public/portraits"
+##  run "ln -nfs #{shared_path}/uploads #{current_path}/public/images/uploads"
+##  run "ln -nfs #{shared_path}/portraits #{current_path}/public/portraits"
+end
+
+# mod_rails (phusion_passenger) stuff
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end 
+  
+  [:start, :stop].each do |t|
+    desc "start/stop is not necessary with mod_rails"
+    task t, :roles => :app do 
+      # nothing
+    end
+  end
 end
