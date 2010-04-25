01d42f2f62bd1e6b6b6963a312dacca6b2da6833
Christopher Clay <hello@c3o.org>
2010-04-25 21:02:19 +0200

    Progress on Tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-  has_and_belongs_to_many :auth, :class_name => 'User', :foreign_key => 'user_id'
+  has_and_belongs_to_many :participants, :class_name => 'User', :foreign_key => 'user_id'
   has_many :comments
 end
