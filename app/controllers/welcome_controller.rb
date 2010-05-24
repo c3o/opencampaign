b76d1089a57504f750958db95958a02fe1e034a7
Christopher Clay <hello@c3o.org>
2010-05-25 00:42:47 +0200

    Remove unused parts

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @map = GMap.new("map_div_id")  
-    @map.control_init(:small_map => true)
-    @map.center_zoom_init([48.206371,16.373062], 11)
-    @map.add_map_type_init(GMapType::G_PHYSICAL_MAP)
-    @map.set_map_type_init(GMapType::G_PHYSICAL_MAP)
-  end
-end
