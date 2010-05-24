044178f2875e578960b18aabaff7e7fc433b03d6
Christopher Clay <hello@c3o.org>
2010-05-25 00:48:32 +0200

    Update a probably unused file ;)

--- public/javascripts/map_overlays.js
+++ public/javascripts/map_overlays.js
@@ -1,9 +1,8 @@
 function makePoly(map) {
-  var geoXml = new GGeoXml("http://boinkor.net/tmp/constituencies.kml");
+  var geoXml = new GGeoXml("http://localhost:3000/constituencies.kml");
   map.addOverlay(geoXml);
   map.removeMapType(G_HYBRID_MAP);
   map.removeMapType(G_NORMAL_MAP);
   map.removeMapType(G_SATELLITE_MAP);
-// p.show();
- 
+  // p.show();
 }
