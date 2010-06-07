40124331b4759006ad0886d521afab2fd0295e22
Christopher Clay <hello@c3o.org>
2010-06-07 05:42:26 +0200

    Restore events page, allow for no constituency

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @constituency = current_user.town.constituency if logged_in?
+    @constituency = current_user.town.constituency if logged_in? && current_user.town
     @events = Event.find(:all, :conditions => ['time > ?', Time.now], :order => 'time ASC')
   end
 end
