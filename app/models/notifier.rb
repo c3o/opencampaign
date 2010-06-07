52ba74c3120a07acf10b308e7784271f3477434d
Christopher Clay <hello@c3o.org>
2010-06-07 05:41:23 +0200

    Further generalization (emails)

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,14 +2,14 @@ class Notifier < ActionMailer::Base
   def signup_thanks(user, password)
     @password = password
     recipients user.email
-    from "LIF <dialog@lif.at>"
-    subject "Diesmal LIF: Danke für deine Unterstützung!"
+    from CONFIG['org_title']+' <'+CONFIG['contact_email']+'>'
+    subject CONFIG['email_signup_subject']
     body :user => user, :password => password
   end
 
   def pass_on(to, sender, message)
     from "#{sender.name} <#{sender.email}>"
-    subject "Diesmal-LIF.at"
+    subject CONFIG['email_refer_subject']
     recipients to
     body :sender => sender, :message => message
   end
