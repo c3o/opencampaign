0760c5bdf3aa399c5b911ac34e9c91d83c3f978f
Christopher Clay <hello@c3o.org>
2010-07-09 05:37:37 +0200

    Static pages: Fix loading

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,12 @@
 class StaticController < ApplicationController
 
   def index
-    if template_exists? path = 'static/' + params[:path].join('/')
-      render :template => path
-    elsif template_exists? path += '/index'
-      render :template => path
-    elsif @page = Page.find_by_path(request.path)
+    if @page = Page.find_by_path(request.path)
       render :template => 'static/page'
+    elsif params[:path] && template_exists?(path = 'static/' + params[:path].join('/'))
+      render :template => path
+    elsif path && template_exists?(path += '/index')
+      render :template => path
     else
       raise ::ActionController::RoutingError,
             "Recognition failed for #{request.path.inspect}"
